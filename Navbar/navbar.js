abbed021279d54e950ac8fcda4a80656582e8d12
Idan Badin <badinidan@gmail.com>
2023-12-03 14:31:57 +0200

    wrvrwv

--- Navbar/navbar.js
+++ Navbar/navbar.js
@@ -20,15 +20,19 @@ document.addEventListener("DOMContentLoaded", function () {
     // Insert the navigation bar HTML into the body
     document.body.insertAdjacentHTML('afterbegin', navbarHtml);
 
-    const currentPage = window.location.href;
+    // Apply initial styles with a short delay
+    object.onload = function(){    setTimeout(function () {
+        const currentPage = window.location.href;
 
-    // Update the active button based on the current page
-    const navbarButtons = document.querySelectorAll('.navbar-button');
-    navbarButtons.forEach(button => {
-        const buttonLink = button.querySelector('a').href;
-        if (currentPage.includes(buttonLink)) {
-            button.classList.add('active');
-            button.querySelector('.highlight').style.width = '100%';
-        }
-    });
+        // Update the active button based on the current page
+        const navbarButtons = document.querySelectorAll('.navbar-button');
+        navbarButtons.forEach(button => {
+            const buttonLink = button.querySelector('a').href;
+            if (currentPage.includes(buttonLink)) {
+                button.classList.add('active');
+                button.querySelector('.highlight').style.width = '100%';
+            }
+        });
+    }, 1000); // Adjust the delay as needed
+    }
 });

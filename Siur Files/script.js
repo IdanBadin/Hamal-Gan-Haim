423b147ac71c65ec19354251cc0c2811af71f5ee
Idan Badin <badinidan@gmail.com>
2024-03-12 16:34:01 +0200

    Bottom div added to siurTables

--- Siur Files/script.js
+++ Siur Files/script.js
@@ -57,6 +57,7 @@ const firebaseConfig = {
               <tr class="spaceUnder"><td id="hour">${cellData[6] || '06:00 - 04:00'}</td><td>${cellData[7] || ''}</td><td>${cellData[8] || ''}</td></tr>
             </tbody>
           </table>
+          <div style="height: 15px; border-radius:0 0 10px 10px; background-color: #9AD0C2"></div>
         </div>`;
     }).join("<br>");
   
